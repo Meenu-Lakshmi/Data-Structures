b2a744fb8292e7e60977be9c0cfcdc9a86250f5d
Meenu-Lakshmi <138270896+Meenu-Lakshmi@users.noreply.github.com>
2023-11-11 18:26:00 +0530

    Create linearqueue.c

---
+++ linearqueue.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdbool.h>
+#define Max_size 5
+int Q[Max_size];
+int front=-1,rear=-1,data,item;
+bool is_full(){
+    if(rear==Max_size-1)
+        return true;
+    else
+        return false;   
+}
+bool is_empty(){
+    if(front==-1)
+        return true;
+    else 
+        return false;   
+}
+void Enqueue(int data){
+	if(is_full()){
+        printf("\nQueue full");
+    }
+    else if (front==-1 && rear==-1){
+            front=0;
+            Q[++rear]=data;
+    }   
+    else{
+        Q[++rear]=data;
+    } 
+}    
+void Dequeue(){   
+	if(is_empty()){
+        printf("\nQueue empty");
+        return;
+    }
+    item=Q[front]; 
+    printf("\nDeleted element : %d\n",item); 
+    if(front==rear){
+        front=-1;   
+        rear=-1;  
+    }
+    else{
+        front=front+1;
+    }
+}
+void Display_queue(){
+	if(is_empty())
+        printf("\nQueue empty");
+    else{    
+	    for (int i = front; i<=rear; i++)
+            printf("%d\n",Q[i]);
+	} 
+}
+int main(){
+    int op;
+    printf("1.Enqueue\n2.Dequeue\n3.Display_queue\n");
+    while(true){
+        printf("\nEnter the Option (Press 0 to exit): ");
+	    scanf("%d",&op);
+        if(op!=0){
+            switch(op){
+                case 1:printf("\nEnter data:");
+                        scanf("%d",&data);
+                        Enqueue(data);
+                        break;
+                case 2:Dequeue();
+                    break;
+                case 3:Display_queue();  
+                    break;      
+            }
+        }
+        else{
+            break;
+        }
+    }
+	return 0;
+}
