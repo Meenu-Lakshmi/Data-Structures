0509df3895c69f9768c7f8c50eac035677ff08d6
Meenu-Lakshmi <138270896+Meenu-Lakshmi@users.noreply.github.com>
2024-02-12 15:39:31 +0530

    Create SLLMinMax.c

---
+++ SLLMinMax.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    struct node* next;
+};
+struct node* create_node(int data) {
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->next = NULL;
+    return newnode;
+}
+void insertE(struct node** head, int data) {
+    struct node* newnode = create_node(data);
+    if (newnode == NULL) {
+        printf("Memory allocation failed\n");
+    }
+    else{
+        if (*head == NULL) {
+            *head = newnode;
+            return;
+        }
+        struct node* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newnode;
+    }
+}
+void MinMax(struct node*head){
+    int min=0,max=0;
+    while(head!=NULL){
+        if(head->data<min){
+            min=head->data;
+        }
+        if(head->data>max){
+            max=head->data;
+        }
+        head=head->next;
+    }
+    printf("Min : %d\n",min);
+    printf("Max : %d\n",max);
+}
+
+void printlist(struct node* head) {
+    printf("Linked list: ");
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+int main() {
+    struct node* head = NULL;
+    int n,a;
+    printf("Enter number of terms:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+    printf("\nElement: ");
+    scanf("%d",&a);
+    insertE(&head, a);
+    }
+    printlist(head);
+    MinMax(head);
+    return 0;
+}
